0480a11e6907c6f3bdde16838eb6328c5873fbd8
Patricia Faustino <patriciafaustinobr@gmail.com>
2025-03-05 20:34:03 -0300

    feat: add API functions for sign-out, update profile, get managed restaurant, and get orders; implement order status component and enhance order table row

--- 04-aprofundando_conceitos_react/src/components/account-menu.tsx
+++ 04-aprofundando_conceitos_react/src/components/account-menu.tsx
@@ -1,61 +1,100 @@
-import { Building, ChevronDown, LogOut } from "lucide-react";
-import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
-import { useQuery } from "@tanstack/react-query";
-import { getProfile } from "@/api/get-profile";
-import { getManagedRestaurant } from "@/api/get-managed-restauraant";
-import { Skeleton } from "./ui/skeleton";
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
+
+import { StoreProfileDialog } from './store-profile-dialog'
+import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
+import { signOut } from '@/api/sign-out'
+import { useNavigate } from 'react-router-dom'
+import { useQuery, useMutation } from '@tanstack/react-query'
 
 export function AccountMenu() {
-    const { data: profile, isLoading: isLoadingProfile} = useQuery({
-        queryKey: ['profile'],
-        queryFn: getProfile,
-      });
+  const navigate = useNavigate();
 
-      const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
-        queryKey: ['managed-restaurant'],
-        queryFn: getManagedRestaurant,
-      });
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  });
 
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    {isLoadingManagedRestaurant ? (
-                        <Skeleton className="h-4 w-40" />
-                    ) : (
-                        managedRestaurant?.name
-                    )}
-                    <ChevronDown className="w-4 h-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col pb-2">
-                    {isLoadingProfile ? (
-                        <div className="space-y-1.5">
-                        <Skeleton className="h-4 w-32" />
-                        <Skeleton className="h-3 w-24" />
-                        </div>
-                    ) : (
-                        <>
-                        <span>{profile?.name}</span>
-                        <span className="text-xs font-normal text-muted-foreground">
-                            {profile?.email}
-                        </span>
-                        </>
-                    )}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                {/* <DropdownMenuItem className="flex">
-                    <Building className="mr-2 h-4 w-4" />
-                    <span>{profile?.email}</span>
-                </DropdownMenuItem> */}
-                <DropdownMenuItem className="flex text-rose-500 dark:text-rose-400">
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+      staleTime: Infinity,
+  });
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true });   
+    }
+  });
+
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex select-none items-center gap-2"
+          >
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem
+            className="text-rose-500 dark:text-rose-400"
+            asChild
+            disabled={isSigningOut}
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
+  )
 }
--- 04-aprofundando_conceitos_react/src/pages/orders/order-table-row.tsx
+++ 04-aprofundando_conceitos_react/src/pages/orders/order-table-row.tsx
@@ -4,10 +4,22 @@ import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
 
+import { OrderStatus } from '@/components/order-status'
 import { OrderDetails } from './order-details'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
+export interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+}
+}
 
-export function OrderTableRow() {
+export function OrderTableRow({order}: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -23,17 +35,23 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        821e78f7asdhdf128h
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="text-muted-foreground">{formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}</TableCell>
       <TableCell>
         <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">Pendente</span>
+          <OrderStatus status={order.status} />
         </div>
       </TableCell>
-      <TableCell className="font-medium">Diego Schell Fernandes</TableCell>
-      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">{order.total.toLocaleString('pt-Br', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
